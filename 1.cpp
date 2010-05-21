de35757fc7ccf92ed7f5ad385223ac05269c341c
mickekb <mk_mailbox@list.ru>
2010-05-21 06:11:33 -0700

    second

--- 1.cpp
+++ 1.cpp
@@ -3,7 +3,21 @@
 #include <unistd.h>
 #include <signal.h>
 
+static void signal_check(int num){
+	printf("Signal recieved");
+	signal(SIGINT, signal_check);
+}
+
 int main(){
-	printf("Signal number %d", 1);
+	signal(SIGINT, signal_check);
+	if(fork()){
+		
+		while(1);
+	}
+	else{
+		setpgid(getpid(),0);
+		while(1);
+		
+	}
 	return 0;
 }
