3bb5c1e62d454d4e67adaa3b4df7cb75d8e8ee72
mickekb <mk_mailbox@list.ru>
2010-05-22 01:59:43 -0700

    90%

--- 2.cpp
+++ 2.cpp
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
 //string format: p1 > p2 > p3
+int pipes[2][2];
+int proc1, proc2, proc3;
+
+void close_pipe(int num){
+	close(pipes[num][0]);
+        close(pipes[num][1]);
+}
 
 int main(){
 	char* my_input = new char[255];
@@ -131,11 +139,80 @@ int main(){
 		
 		if(!err){
 			printf("Correct input; p1: %s, p2: %s, p3: %s \n", p1, p2, p3);
+			//set pipes
+			if (pipe(pipes[0])) {
+			    printf("Cant open first pipe");
+			    continue;
+			}
+
+			if (pipe(pipes[1])) {
+			    printf("Cant open second pipe");
+			    close_pipe(0);
+			    continue;
+			}
+			proc1 = fork();
+			if(proc1==0){
+				if (dup2(pipes[0][1], STDOUT_FILENO)==-1){
+					printf("Cant dup p1");
+					return 0;
+				}
+				if (dup2(pipes[1][1], STDERR_FILENO)==-1){
+					printf("Cant dup p1");
+					return 0;
+				}
+				close_pipe(1);
+				execlp(p1,p1,NULL);
+				exit(0);
+			}
+			else if(proc1==-1){
+				printf("Cant fork");
+				close_pipe(0);
+				close_pipe(1);
+				continue;
+			}	
+
+			proc2 = fork();
+			if(!proc2){
+				if (dup2(pipes[1][0], STDIN_FILENO)==-1){
+					printf("Cant dup p2");
+					return 0;
+				}
+				close_pipe(1);
+				execlp(p2,p2,NULL);
+				return 0;
+			}
+			else if(proc2==-1){
+				printf("Cant fork 2");
+				close_pipe(0);
+				close_pipe(1);
+				continue;
+			}
+
+			proc3 = fork();
+			if(!proc3){
+				if (dup2(pipes[0][0], STDIN_FILENO)==-1){
+					printf("Cant dup p3");
+					return 0;
+				}
+				close_pipe(1);
+				execlp(p3,p3,NULL);
+				return 0;
+			}
+			else if(proc3==-1){
+				printf("Cant fork 3");
+				close_pipe(0);
+				close_pipe(1);
+				continue;
+			}	
 		}
 		else{
 			printf("Wrong format. Right is: p1 > p2 > p3\n");
 		}
-
+		int status;
+		wait(&status);
+		wait(&status);
+		wait(&status);
+		printf("Success!\n");
 	}	
 	return 0;
 }
